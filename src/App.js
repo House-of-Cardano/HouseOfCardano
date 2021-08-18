6cd5a946c71cc2b9ecb597542f7d1652320abaad
simonholmes001 <simonholmesabc@gmail.com>
2021-08-18 21:33:56 +0200

    trials to load json metadata to the website

--- src/App.js
+++ src/App.js
@@ -14,17 +14,15 @@ function App() {
   );
 }
 
-fetch("./poolMetadata.json", {
-    headers : { 
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  }
-})
-  .then(function(resp) {
-      return resp.json();
-  })
-  .then(function(data) {
-      console.log(data);
-  })
+fetch('../public/poolMetaData.json').then(response => {
+    console.log(response);
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+    console.log(data);
+  }).catch(err => {
+    // Do something for an error here
+    console.log("Error Reading data " + err);
+  });
 
 export default App;
